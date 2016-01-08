013b19087f17624d7f7e8058bcc0dccc49740b68
Abuzer Durmus <abuzer.durmus02@gmail.com>
2016-01-08 16:24:30 +0200

    ethetr

--- controller/securityTerminalController.js
+++ controller/securityTerminalController.js
@@ -1,22 +1,22 @@
-mavikentApp.controller("SecturityTerminalCtrl", function($scope, $state,$http, $rootScope, $interval) {
+mavikentApp.controller("SecturityTerminalCtrl", function ($scope, $state, $http, $rootScope, $interval) {
     $scope.istekler = [];
-     $scope.host=host;
-    $scope.cikis=function(){
+    $scope.host = host;
+    $scope.cikis = function () {
         $state.go("logout");
     }
-    var security = $interval(function() {
+    var security = $interval(function () {
         $http.post(host + "/api/emergency/search?token=" + $rootScope.mkb.token, {
             floor_no: $rootScope.mkb.current_user.floor_id.name,
             isActive: "false"
-        }).success(function(resp) {
+        }).success(function (resp) {
             if (resp.status == false) {
                 console.log("error : ", JSON.stringify(resp));
-                stateControl(resp.code,resp.data);
+                stateControl(resp.code, resp.data);
                 $interval.cancel(security);
                 return;
             }
             $scope.istekler = resp.data;
-        }).error(function(err) {
+        }).error(function (err) {
             console.log(JSON.stringify(err));
             sweetAlert("Oops...", "Bir hata oluştu", "error");
             $interval.cancel(security);
@@ -25,27 +25,21 @@ mavikentApp.controller("SecturityTerminalCtrl", function($scope, $state,$http, $
     }, 1000);
 
 
-    $scope.istekAl = function(item, index) {
+    $scope.istekAl = function (item, index) {
         $http.put(host + "/api/emergency?token=" + $rootScope.mkb.token, {
-                _id: item._id,
-                isActive: true
-            }).success(function(resp) {
-                if (resp.status == false) {
-                    console.log("error : ", JSON.stringify(resp));
-                    stateControl(resp.code,resp.data);
-                    return;
-                }
-                console.log(resp, undefined, 4);
-                swal("Başarılı!", "İstek Başarılı ile Alındı!", "success")
-            }).error(function(err) {
-                console.log(JSON.stringify(err));
+            _id: item._id,
+            isActive: true
+        }).success(function (resp) {
+            if (resp.status == false) {
+                console.log("error : ", JSON.stringify(resp));
+                stateControl(resp.code, resp.data);
+                return;
+            }
+            console.log(resp, undefined, 4);
+            swal("Başarılı!", "İstek Başarılı ile Alındı!", "success")
+        }).error(function (err) {
+            console.log(JSON.stringify(err));
             sweetAlert("Oops...", "Bir hata oluştu", "error");
-            })
-            /*
-             $http.delete(host+"/api/service/"+item._id+"?token="+$rootScope.mkb.token).success(function(resp){
-                console.log(resp,undefined,4);
-            }).error(function(err){
-                console.log(JSON.stringify(err));
-            })*/
+        })
     }
 })
