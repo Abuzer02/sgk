8df8cbcce9004de79991ae8ea076797eab354d2f
Abuzer Durmus <abuzer.durmus02@gmail.com>
2016-01-12 23:30:33 +0200

    dkdddrg

--- controller/authController.js
+++ controller/authController.js
@@ -1,37 +1,72 @@
 mavikentApp.controller('authController', AuthController);
-function AuthController ($scope,$auth, $state, $rootScope, $localStorage) {
-  var vm = this
-  if ($localStorage['token']) {
-    if (!$rootScope.mkb.token) {
-      $rootScope.mkb.token = $localStorage['token']
-    }
-  }
 
-  vm.login = function() {
-    var credentials = {
-      username : vm.username,
-      password : vm.password
-    }
-    $auth.login(credentials).then(function(response) {
-        if (response.data.state) {
-          $scope.message=stateControl(response.data.code);
-         if(!$localStorage["token"]){
-             $localStorage["token"]=response.data.token;
-          }
-         if(!$localStorage["user"]){
-             $localStorage["user"]=response.data.data;
-         }
-            if(!$localStorage["navigation_url"]){
-             $localStorage["navigation_url"]=response.data.navigation_url;
-         }
-          $rootScope.mkb.token = response.data.token
-          $rootScope.mkb.navigation_url = response.data.navigation_url
-          $rootScope.mkb.current_user = response.data.data
-          $state.go($rootScope.mkb.navigation_url);
-          
-        }else{
-           $scope.message=stateControl(response.data.code);
+function AuthController($scope, $auth, $state, $rootScope, $localStorage, $http) {
+    var vm = this
+    if ($localStorage['token']) {
+        if (!$rootScope.mkb.token) {
+            $rootScope.mkb.token = $localStorage['token']
         }
-    });
-  }
+    }
+
+    vm.login = function() {
+        var credentials = {
+            username: vm.username,
+            password: vm.password
+        }
+        $auth.login(credentials).then(function(response) {
+            console.log(response);
+            if (response.data.state) {
+                $scope.message = stateControl(response.data.code);
+                if (!$localStorage["token"]) {
+                    $localStorage["token"] = response.data.token;
+                }
+                if (!$localStorage["user"]) {
+                    $localStorage["user"] = response.data.data;
+                }
+                if (!$localStorage["navigation_url"]) {
+                    $localStorage["navigation_url"] = response.data.navigation_url;
+                }
+                $rootScope.mkb.token = response.data.token
+                $rootScope.mkb.navigation_url = response.data.navigation_url
+                $rootScope.mkb.current_user = response.data.data
+                $state.go($rootScope.mkb.navigation_url);
+
+            } else {
+                $scope.message = stateControl(response.data.code);
+            }
+        });
+    }
+    $scope.showModal = false;
+    $scope.toggleModal = function() {
+        $scope.showModal = !$scope.showModal;
+    };
+    $scope.newPass = {
+        email: "",
+        password: ""
+    }
+    $scope.updatePass = function() {
+        $http.put(host + "/forgot/password", $scope.newPass).success(function(resp) {
+            if (resp.status == false) {
+                console.log("error : ", JSON.stringify(resp));
+                stateControl(resp.code, resp.data);
+                return;
+            }
+            $scope.newPass = {
+                email: "",
+                password: ""
+            }
+            console.log(resp.data);
+            $scope.showModal = false;
+            swal({
+                title: "Başarılı",
+                text: "şifre değişimi başarılı giriş sayfasına yönlendiriliyorsunuz",
+                type: "success",
+                timer: 1000,
+                showConfirmButton: false
+            });
+        }).error(function(err) {
+            console.error(JSON.stringify(err));
+            sweetAlert("Oops...", "Bir hata oluştu", "error");
+        })
+    }
 }
